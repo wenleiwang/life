4ac49136d681a46634dd306e5edf1ed868cf3e87
wangwenlei <wenleiwang@126.com>
2020-09-21 22:53:45 +0800

    <文档更新>(学习IDEA如何使用GIT):创建多个分支，让GIT版本画出多条线

---
+++ src/main/java/ink/poesy/life/controller/GitTestController.java
@@ -0,0 +1,10 @@
+package ink.poesy.life.controller;
+
+/**
+ * @ClassName GitTestController
+ * @Author wwl
+ * @Date 2020/9/21 22:15
+ * @Version 1.0
+ **/
+public class GitTestController {
+}
---
+++ src/main/java/ink/poesy/life/controller/MasterGitTest.java
@@ -0,0 +1,10 @@
+package ink.poesy.life.controller;
+
+/**
+ * @ClassName MasterGitTest
+ * @Author wwl
+ * @Date 2020/9/21 22:27
+ * @Version 1.0
+ **/
+public class MasterGitTest {
+}
