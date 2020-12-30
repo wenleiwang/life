b2176dfe3401676bd6797699ec76498e4e21e050
wangwenlei <wangwenlei@fang.com>
2020-12-30 14:15:10 +0800

    <文档改动>：(Blog):更新数据表结构

--- life-blog/src/main/resources/databases/poesy_life.sql
+++ life-blog/src/main/resources/databases/poesy_life.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50731
  File Encoding         : 65001
 
- Date: 06/11/2020 10:58:44
+ Date: 30/12/2020 14:05:06
 */
 
 SET NAMES utf8mb4;
@@ -29,31 +29,31 @@ CREATE TABLE `blog_article`  (
   `user_id` int(11) NOT NULL COMMENT '文章所属用户',
   `add_time` datetime(0) NULL DEFAULT NULL,
   `update_time` datetime(0) NULL DEFAULT NULL,
-  `deleted` tinyint(1) UNSIGNED ZEROFILL NULL DEFAULT NULL,
-  `star_num` int(11) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '点赞数量',
-  `comment_num` int(11) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '评论数量',
-  `collect_num` int(11) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '收藏数量',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '0-发布；1-删除；2-草稿',
+  `star_num` int(11) UNSIGNED NULL DEFAULT NULL COMMENT '点赞数量',
+  `comment_num` int(11) NULL DEFAULT NULL COMMENT '评论数量',
+  `collect_num` int(11) NULL DEFAULT NULL COMMENT '收藏数量',
   `star_status` tinyint(1) NULL DEFAULT NULL COMMENT '是否开启点赞',
-  `comment_status` tinyint(1) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '是否开启评论',
+  `comment_status` tinyint(1) NULL DEFAULT NULL COMMENT '是否开启评论',
   `collect_status` tinyint(1) NULL DEFAULT NULL COMMENT '是否开启收藏',
   `article_img_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '首图',
-  `artcle_flag` int(2) NULL DEFAULT NULL COMMENT '文章标志 0-原创；1-转载；2-翻译；',
+  `article_flag` int(2) NULL DEFAULT NULL COMMENT '文章标志 0-原创；1-转载；2-翻译；',
   PRIMARY KEY (`article_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for blog_classify
 -- ----------------------------
 DROP TABLE IF EXISTS `blog_classify`;
 CREATE TABLE `blog_classify`  (
-  `classify_id` int(11) UNSIGNED ZEROFILL NOT NULL COMMENT '分类ID',
+  `classify_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类ID',
   `classify_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '分类名',
   `user_id` int(11) NULL DEFAULT NULL COMMENT '分类所属用户',
   `add_time` datetime(0) NULL DEFAULT NULL COMMENT '添加时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  `deleted` tinyint(1) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '是否删除',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
   PRIMARY KEY (`classify_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for blog_comment
@@ -62,14 +62,28 @@ DROP TABLE IF EXISTS `blog_comment`;
 CREATE TABLE `blog_comment`  (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '评论ID',
   `comment_body` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '评论内容',
-  `comment_parent` int(11) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '父级评论ID，0-表示1级评论',
+  `comment_parent` int(11) NULL DEFAULT NULL COMMENT '父级评论ID，0-表示1级评论',
   `user_id` int(11) NULL DEFAULT NULL COMMENT '评论用户',
   `article_id` int(11) NULL DEFAULT NULL COMMENT '被评论文章',
   `add_time` datetime(0) NULL DEFAULT NULL COMMENT '评论添加时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '评论修改时间',
-  `deleted` tinyint(1) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '是否删除',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
   PRIMARY KEY (`comment_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for blog_res_article_classify
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_res_article_classify`;
+CREATE TABLE `blog_res_article_classify`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `article_id` int(11) NOT NULL COMMENT '文章ID',
+  `classify_id` int(11) NOT NULL COMMENT '分类ID',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  `add_time` datetime(0) NULL DEFAULT NULL COMMENT '添加数据时间',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改数据时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for blog_tag
@@ -81,7 +95,7 @@ CREATE TABLE `blog_tag`  (
   `user_id` int(11) NULL DEFAULT NULL COMMENT '标签所属用户',
   `add_time` datetime(0) NULL DEFAULT NULL COMMENT '标签添加时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '标签修改时间',
-  `deleted` tinyint(1) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '是否删除',
+  `deleted` tinyint(1) UNSIGNED NULL DEFAULT NULL COMMENT '是否删除',
   PRIMARY KEY (`tag_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
@@ -96,11 +110,12 @@ CREATE TABLE `blog_user`  (
   `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名',
   `real_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `user_age` int(3) NULL DEFAULT NULL COMMENT '虚拟年龄',
-  `real_age` int(3) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '真实年龄',
+  `real_age` int(3) NULL DEFAULT NULL COMMENT '真实年龄',
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',
   `add_time` datetime(0) NULL DEFAULT NULL COMMENT '插入数据时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改数据时间',
-  `deleted` tinyint(1) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '删除状态',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除状态',
+  `user_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户头像',
   PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
