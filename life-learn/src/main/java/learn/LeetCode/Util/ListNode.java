0f03466ae2a036feda7d95eddee349e8095b6d17
wangwenlei <wangwenlei@fang.com>
2021-06-10 15:57:05 +0800

    <LeetCode>：(learn):002

---
+++ life-learn/src/main/java/learn/LeetCode/Util/ListNode.java
@@ -0,0 +1,13 @@
+package learn.LeetCode.Util;
+
+/**
+ * @author WangWenLei
+ * @DATE: 2021/6/9
+ **/
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
