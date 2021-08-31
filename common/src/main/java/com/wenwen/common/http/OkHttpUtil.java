376a59344fe533705559dd974079fc09d27f037b
wangwenlei <wangwenlei@fang.com>
2021-08-31 14:13:12 +0800

    <架构>：（post）：添加POST模块

---
+++ common/src/main/java/com/wenwen/common/http/OkHttpUtil.java
@@ -0,0 +1,145 @@
+package com.wenwen.common.http;
+
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @author WangWenLei
+ * @DATE: 2021/8/30
+ **/
+@Slf4j
+@Component
+public class OkHttpUtil {
+    @Resource
+    private OkHttpClient okHttpClient;
+    /**
+     * get
+     *
+     * @param url     请求的url
+     * @param queries 请求的参数，在浏览器？后面的数据，没有可以传null
+     * @return
+     */
+    public String get(String url, Map<String, String> queries) {
+        String responseBody = "";
+        StringBuffer sb = new StringBuffer(url);
+        if (queries != null && queries.keySet().size() > 0) {
+            boolean firstFlag = true;
+            Iterator iterator = queries.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry<String, String>) iterator.next();
+                if (firstFlag) {
+                    sb.append("?" + entry.getKey() + "=" + entry.getValue());
+                    firstFlag = false;
+                } else {
+                    sb.append("&" + entry.getKey() + "=" + entry.getValue());
+                }
+            }
+        }
+        Request request = new Request
+                .Builder()
+                .url(sb.toString())
+                .build();
+        Response response = null;
+        try {
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (status == 200) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            log.error("okhttp put error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return responseBody;
+    }
+    /**
+     * post
+     *
+     * @param url    请求的url
+     * @param params post form 提交的参数
+     * @return
+     */
+    public String post(String url, Map<String, String> params) {
+        String responseBody = "";
+        FormBody.Builder builder = new FormBody.Builder();
+        //添加参数
+        if (params != null && params.keySet().size() > 0) {
+            for (String key : params.keySet()) {
+                builder.add(key, params.get(key));
+            }
+        }
+        Request request = new Request
+                .Builder()
+                .url(url)
+                .post(builder.build())
+                .build();
+        Response response = null;
+        try {
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (status == 200) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            log.error("okhttp post error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return responseBody;
+    }
+    /**
+     * post 上传文件
+     *
+     * @param url
+     * @param params
+     * @param fileType
+     * @return
+     */
+    public String postFile(String url, Map<String, Object> params, String fileType) {
+        String responseBody = "";
+        MultipartBody.Builder builder = new MultipartBody.Builder();
+        //添加参数
+        if (params != null && params.keySet().size() > 0) {
+            for (String key : params.keySet()) {
+                if (params.get(key) instanceof File) {
+                    File file = (File) params.get(key);
+                    builder.addFormDataPart(key, file.getName(), RequestBody.create(MediaType.parse(fileType), file));
+                    continue;
+                }
+                builder.addFormDataPart(key, params.get(key).toString());
+            }
+        }
+        Request request = new Request
+                .Builder()
+                .url(url)
+                .post(builder.build())
+                .build();
+        Response response = null;
+        try {
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (status == 200) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            log.error("okhttp postFile error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return responseBody;
+    }
+}
---
+++ life-learn/src/main/java/learn/algorithm/search/Performance.java
@@ -0,0 +1,94 @@
+package learn.algorithm.search;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * @author WangWenLei
+ * @DATE: 2021/8/7
+ **/
+public class Performance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Student> dataList = new ArrayList<>();
+        for(int i = 0 ; i < 3 ; i++){
+            String s = sc.nextLine();
+            String[] s1 = s.split(" ");
+            Student stu = new Student();
+            stu.setName(s1[0]);
+            stu.setCh(Integer.parseInt(s1[1]));
+            stu.setEn(Integer.parseInt(s1[2]));
+            stu.setM(Integer.parseInt(s1[3]));
+            stu.setSum(Integer.parseInt(s1[1]) + Integer.parseInt(s1[2]) + Integer.parseInt(s1[3]));
+            dataList.add(stu);
+        }
+        // 已经排好
+        List<Student> collect = dataList.stream().sorted(Comparator.comparing(Student::getSum, Comparator.nullsFirst(Integer::compareTo))
+                .thenComparing(Student::getCh, Comparator.nullsFirst(Comparator.naturalOrder()))
+                .thenComparing(Student::getEn, Comparator.nullsFirst(Comparator.naturalOrder()))
+                .thenComparing(Student::getM, Comparator.nullsFirst(Comparator.naturalOrder()))
+                .thenComparing(Student::getName, Comparator.nullsFirst(Comparator.naturalOrder()))
+                .reversed())
+                .collect(Collectors.toList());
+
+        System.out.println(collect.toString());
+
+    }
+
+
+    static class Student{
+        private String name ;
+        private Integer ch;
+        private Integer en;
+        private Integer m;
+        private Integer sum;
+
+        public int add(){
+            return this.ch + this.en + this.m;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getCh() {
+            return ch;
+        }
+
+        public void setCh(Integer ch) {
+            this.ch = ch;
+        }
+
+        public Integer getEn() {
+            return en;
+        }
+
+        public void setEn(Integer en) {
+            this.en = en;
+        }
+
+        public Integer getM() {
+            return m;
+        }
+
+        public void setM(Integer m) {
+            this.m = m;
+        }
+
+        public Integer getSum() {
+            return sum;
+        }
+
+        public void setSum(Integer sum) {
+            this.sum = sum;
+        }
+    }
+}
