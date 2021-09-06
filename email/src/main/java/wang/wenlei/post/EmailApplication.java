205f36dbe557b8580e137666b616c85749f53510
wangwenlei <wangwenlei@fang.com>
2021-09-06 09:59:34 +0800

    <文档改动>：（post）：模块调整

--- email/src/main/java/wang/wenlei/post/EmailApplication.java
+++ email/src/main/java/wang/wenlei/post/EmailApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostApplication {
+public class EmailApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PostApplication.class, args);
+		SpringApplication.run(EmailApplication.class, args);
 	}
 
 }
