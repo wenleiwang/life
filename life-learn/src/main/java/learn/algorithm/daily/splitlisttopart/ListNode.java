5f2b65f27830692bc92f87e32194fe4bb8acf41d
wangwenlei <wangwenlei@fang.com>
2021-10-08 09:28:07 +0800

    <每日一练>：(算法):切分数组

---
+++ life-learn/src/main/java/learn/algorithm/daily/splitlisttopart/ListNode.java
@@ -0,0 +1,19 @@
+package learn.algorithm.daily.splitlisttopart;
+
+/**
+ * 节点
+ * @author WangWenLei
+ * @DATE: 2021/9/23
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode (){}
+    ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
